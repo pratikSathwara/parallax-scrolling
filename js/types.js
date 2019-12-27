fcb48cbcf25ea6a127b5b42011fd6b767eb93602
pratik sathawara <sathavarapratik76@gmail.com>
2019-12-27 18:56:12 +0530

    change

---
+++ js/types.js
@@ -0,0 +1,47 @@
+// typescript is a programming language
+//  maintained by microsoft 
+// it is a superset of javascript
+//  typescript developer we must convert  typescript code into javascript code.
+// datatype : number , array , string , any boolean
+// type Assertion means how many types of define the variable 
+// let name ;
+// name = "pratik";
+// (<string>name).length;
+// (name as string).length;
+// document.write(name);
+// => arrow function
+//  it is called the fat arrow (because -> is a thin arrow
+// and => is a fat arrow ) 
+// var pratik = function(cube){
+//     document.write(cube);
+// }
+// pratik("");
+// var pratik = (cube) => document.write(cube);
+// pratik("hello")
+// interface 
+// interface Nanepro {
+//     firstname:string ,
+//     lastname:string,
+//     age : number
+// }
+// let names = (namepro: Nanepro) {
+//     document.write(namepro.firstname + " " + namepro.lastname + " " + namepro.age);
+// }
+// names({
+//     firstname:"pratik",
+//     lastname:"sathwara",
+//     age:1,
+// });
+//  class is a collection of data type 
+//  to create an instance of the class , use the new keyword
+//  var Object_name = new class_name();
+// class youtube{
+//     constructor(public firstname:string ,private lastname?:string ,public age?:number){
+//     }
+//     getFirstname(){
+//         document.write(this.firstname +" "+ this.lastname +" "+ this.age);
+//     }
+// }
+// let y_obj = new youtube("pratik", "sathawara");
+// y_obj.age = 10;
+// y_obj.getFirstname();
